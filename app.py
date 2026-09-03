e9d6121ac617c6374de6a5c8310da4c82ecebd1f
wgj543 <wgj543@gmail.com>
2026-09-04 00:30:31 +0800

    fix render port

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask
+import os
 
 app = Flask(__name__)
 
@@ -7,4 +8,5 @@ def home():
     return "Stock Bot Running!"
 
 if __name__ == "__main__":
-    app.run(port=5000)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
