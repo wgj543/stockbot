61da96d32fb1868e797668b3e02aa6d2dbc102a0
wgj543 <wgj543@gmail.com>
2026-09-04 00:49:30 +0800

    show line event

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, request
+import os
 
 app = Flask(__name__)
 
@@ -8,9 +9,9 @@ def home():
 
 @app.route("/callback", methods=["POST"])
 def callback():
+    print(request.json)
     return "OK"
 
 if __name__ == "__main__":
-    import os
     port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
